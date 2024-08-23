1aa56b3c821402cecbd0bbbd2da3d318ff8cc997
Iliyan Ivanov <iliyan.ivanov9428@gmail.com>
2024-08-23 19:05:38 +0300

    Some changes in config file

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const PORT = 5555;
+export const PORT = process.env.PORT || 5555;
 
-export const mongoDBURL = "mongodb+srv://iliqn4o94288:nokia6120@movies.abvr8.mongodb.net/movies-collection?retryWrites=true&w=majority&appName=Movies";
+export const mongoDBURL = process.env.MONGO_URI || "mongodb://localhost:27017/movies-collection";
